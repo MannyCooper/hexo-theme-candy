671da364c6839c82d7c000a4737d3aa7031dd6ea
MannyCooper <an1998821@gmail.com>
2020-11-09 14:55:54 +0800

    (feat) Support dark mode img in content

--- source/js/dark.js
+++ source/js/dark.js
@@ -1,50 +1,77 @@
-(function() {
-    var isDark = localStorage.getItem('dark');
+(function () {
+    function switchMode() {
+        if (media.matches) {
+            document.body.classList.add("dark");
+            localStorage.setItem("dark", true);
+        } else {
+            document.body.classList.remove("dark");
+            localStorage.setItem("dark", false);
+        }
+        var darkLight = {true: 'dark', false: 'light'};
+        setPicturesThemed(darkLight[media.matches]);
+    }
+
+    let media = window.matchMedia("(prefers-color-scheme: dark)");
+    switchMode();
+    let callback = (e) => {
+        switchMode();
+    };
+
+    if (typeof media.addEventListener === "function") {
+        media.addEventListener("change", callback);
+    } else if (typeof media.addListener === "function") {
+        media.addListener(callback);
+    }
+
+    var isDark = localStorage.getItem("dark");
     var darkNav;
 
     function applyDark(value) {
-        if (value.toString() === 'true') {
-            document.body.classList.add('dark');
+        if (value.toString() === "true") {
+            document.body.classList.add("dark");
         } else {
-            document.body.classList.remove('dark');
+            document.body.classList.remove("dark");
         }
     }
 
     function findDarkNav() {
-        darkNav = document.getElementById('dark-nav');
+        darkNav = document.getElementById("dark-nav");
         if (!darkNav) {
             setTimeout(findDarkNav, 100);
         } else {
-            darkNav.addEventListener('click', switchDark);
+            darkNav.addEventListener("click", switchDark);
+            var darkLight = {true: 'dark', false: 'light'};
+            setPicturesThemed(darkLight[isDark]);
         }
     }
 
     function switchDark() {
-        isDark = isDark ? isDark.toString() !== 'true' : true;
+        isDark = isDark ? isDark.toString() !== "true" : true;
         applyDark(isDark);
-        localStorage.setItem('dark', isDark);
+        localStorage.setItem("dark", isDark);
+        var darkLight = {true: 'dark', false: 'light'};
+        setPicturesThemed(darkLight[isDark]);
     }
+
+
+    function setPicturesThemed(colorScheme = undefined) {
+        document.querySelectorAll('picture > source[data-cloned-theme]').forEach(el => {
+        el.remove();
+        });
+        
+        if (colorScheme) {
+        document.querySelectorAll(`picture > source[media*="(prefers-color-scheme: ${colorScheme})"]`).forEach(el => {
+        const cloned = el.cloneNode();
+        cloned.removeAttribute('media');
+        cloned.setAttribute('data-cloned-theme', colorScheme);
+        cloned.setAttribute('class', 'lg_img');
+        el.parentNode.prepend(cloned);
+        el.parentNode.parentNode.setAttribute('href',cloned.srcset);
+        });
+        }
+        }
 
     findDarkNav();
     isDark && applyDark(isDark);
+})();
 
-    let media = window.matchMedia('(prefers-color-scheme: dark)');
-    let prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    if (prefersDarkMode) {
-        document.body.classList.add('dark');
-    }
-    let callback = (e) => {
-        let prefersDarkMode = e.matches;
-        if (media.matches) {
-            document.body.classList.add('dark');
-        } else {
-            document.body.classList.remove('dark');
-        }
-    };
-    if (typeof media.addEventListener === 'function') {
-        media.addEventListener('change', callback);
-    } else if (typeof media.addListener === 'function') {
-        media.addListener(callback);
-    }
-
-}());
