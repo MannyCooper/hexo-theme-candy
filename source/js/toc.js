465b3a7342d99981972075c8d7bd0228be699a69
MannyCooper <an1998821@gmail.com>
2020-11-18 01:58:12 +0800

    Update toc.js
    Upgrade toc function

--- source/js/toc.js
+++ source/js/toc.js
@@ -1,43 +1,91 @@
-$(document).ready(function () {
-    var observerTopMargin;
-    var scrollObserver;
-    var headerElems = $(".headerlink");
-    var activeTocItem;
-
-    function initIntersectionObserver(docHeight) {
-        observerTopMargin = docHeight;
-        scrollObserver = new IntersectionObserver(scrollCallBack,
-            {
-                root: null,  // viewpoint
-                rootMargin: docHeight + "px 0px -80% 0px"  // cover top 30% of viewport to the top of document
-            })
-    }
-    
-    function scrollCallBack(entries, observer) {
-        if ($(window).scrollTop() > observerTopMargin * 0.7) { 
-            // User somehow scroll to 70% of observerTopMargin (which is inited as 200% document height)
-            // Observer top margin need to extend to cover all the space to the top of the document
-            initIntersectionObserver(observerTopMargin * 2)
-            observer.disconnect();
-            return;
-        }
-        let toActive;
-        if (entries[0].intersectionRatio == 1) {  // enter viewed area
-            let entry = entries.reduce((u, v) => (u.target.toc_id > v.target.toc_id ? u : v));  // get the lowest item
-            toActive = $("#toc-item-" + $(entry.target).attr("href").substr(1));
-        } else {
-            let entry = entries.reduce((u, v) => (u.target.toc_id < v.target.toc_id ? u : v));  // get the highest item
-            let idx = Math.max(entry.target.toc_id - 1, 0);
-            toActive = $("#toc-item-" + $(headerElems[idx]).attr("href").substr(1));
-        }
-        if (activeTocItem) activeTocItem.removeClass("is-current");
-        activeTocItem = toActive
-        activeTocItem.addClass("is-current");
+(function (window, document) {
+    function scrollTo(id) {
+        var element = document.getElementById(id);
+        var headerOffset = -20;
+        var elementPosition = element.offsetTop;
+        var offsetPosition = elementPosition - headerOffset;
+        document.documentElement.scrollTop = offsetPosition;
+        document.body.scrollTop = offsetPosition; // For Safari
     }
 
-    initIntersectionObserver($(document).height() * 2);
-    headerElems.each(function (index, obj) {
-        obj.toc_id = index;
-        scrollObserver.observe(obj);
-    })
-});
+    function register($toc) {
+      const currentInView = new Set();
+      const headingToMenu = new Map();
+      const $menus = Array.from($toc.querySelectorAll('.menu-list > li > a'));
+  
+      for (const $menu of $menus) {
+        const elementId = $menu.getAttribute('href').trim().slice(1);
+        const $heading = document.getElementById(elementId);
+
+        if ($heading) {
+          headingToMenu.set($heading, $menu);
+        }
+      }
+  
+      const $headings = Array.from(headingToMenu.keys());
+  
+      const callback = (entries) => {
+        for (const entry of entries) {
+          if (entry.isIntersecting) {
+            currentInView.add(entry.target);
+          } else {
+            currentInView.delete(entry.target);
+          }
+        }
+        let $heading;
+        if (currentInView.size) {
+          // heading is the first in-view heading
+          $heading = [...currentInView].sort(($el1, $el2) => $el1.offsetTop - $el2.offsetTop)[0];
+        } else if ($headings.length) {
+          // heading is the closest heading above the viewport top
+          $heading = $headings
+            .filter(($heading) => $heading.offsetTop < window.scrollY)
+            .sort(($el1, $el2) => $el2.offsetTop - $el1.offsetTop)[0];
+        }
+        if ($heading && headingToMenu.has($heading)) {
+          $menus.forEach(($menu) => $menu.classList.remove('is-active'));
+  
+          const $menu = headingToMenu.get($heading);
+          $menu.classList.add('is-active');
+          let $menuList = $menu.parentElement.parentElement;
+          while (
+            $menuList.classList.contains('menu-list') &&
+            $menuList.parentElement.tagName.toLowerCase() === 'li'
+          ) {
+            $menuList.parentElement.children[0].classList.add('is-active');
+            $menuList = $menuList.parentElement.parentElement;
+          }
+        }
+      };
+      const observer = new IntersectionObserver(callback, { threshold: 0 });
+  
+      for (const $heading of $headings) {
+        observer.observe($heading);
+        // smooth scroll to the heading
+        if (headingToMenu.has($heading)) {
+          const $menu = headingToMenu.get($heading);
+          $menu.setAttribute('data-href', $menu.getAttribute('href'));
+          $menu.setAttribute('href', 'javascript:;');
+          $menu.addEventListener('click', () => {
+            if (typeof $heading.scrollTo === 'function') {
+            //   $heading.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            scrollTo($heading.id);
+            }
+            const anchor = $menu.getAttribute('data-href');
+            if (history.pushState) {
+              history.pushState(null, null, anchor);
+            } else {
+              location.hash = anchor;
+            }
+          });
+          $heading.style.scrollMargin = '1em';
+        }
+      }
+    }
+  
+    if (typeof window.IntersectionObserver === 'undefined') {
+      return;
+    }
+  
+    document.querySelectorAll('#toc').forEach(register);
+  })(window, document);
